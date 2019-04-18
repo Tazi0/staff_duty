edc3af5a6f4dfb7aa504a03817e53c74ee450add
Tazio <tazio2003@gmail.com>
2019-04-18 15:46:09 +0200

    Test and this works!

--- staff_duty/server.lua
+++ staff_duty/server.lua
@@ -4,25 +4,37 @@
 -- add_ace Adminrole taz.staff allow
 -- add_principal identifier.steam:[hexid] Adminrole
 
-list = Set {}
+local people = { }
 
-staff = "taz.staff"
+local staff = "taz.staff"
 
 -- Editing stuff below this line will be at your own risk
 
 RegisterCommand("staffduty",function(source, args)
   local name = GetPlayerName(source) -- Gets player name
-  if IsPlayerAceAllowed(source, staff) and list[name] then
-    TriggerClientEvent('chat:addMessage', -1, { args = { "^7[ ^3Staff Duty ^7] (^3 " .. name.." ^7)", " is now ^8OFF ^7duty" }, color = 255, 0, 0 })
-    table.remove(list, name) --Set table to remove the name
-  elseif IsPlayerAceAllowed(source, staff) and not list[name] then
-    TriggerClientEvent('chat:addMessage', -1, { args = { "^7[ ^3Staff Duty ^7] (^3 " .. name.." ^7)", " is now ^2ON ^7duty"}, color = 255, 0, 0 })
-    table.insert(list, name)
+  if IsPlayerAceAllowed(source, staff) and has_value(people, name) then
+    TriggerClientEvent('chat:addMessage', -1, { args = { " ^7[ ^3Staff Duty ^7] (^3 " .. name.." ^7)", " is now ^8OFF ^7duty" }, color = 255, 0, 0 })
+    removeFirst(people, name) --Set table to remove the name
+  elseif IsPlayerAceAllowed(source, staff) and not has_value(people, name) then
+    TriggerClientEvent('chat:addMessage', -1, { args = { " ^7[ ^3Staff Duty ^7] (^3 " .. name.." ^7)", " is now ^2ON ^7duty"}, color = 255, 0, 0 })
+    table.insert(people, name)
   end
 end, true)
 
-function Set (list)
-  local set = {}
-  for _, l in ipairs(list) do set[l] = true end
-  return set
+function has_value (tab, val)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+
+    return false
+end
+
+function removeFirst(tbl, val)
+  for i, v in ipairs(tbl) do
+    if v == val then
+      return table.remove(tbl, i)
+    end
+  end
 end
