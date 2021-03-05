4e64555588831ce2d7d6bfd8edc4b4cfc709da30
izMystic <mystic@izmystic.dev>
2021-03-05 10:38:41 -0500

    change __resource.lua to fxmanifest.lua

---
+++ staff_duty/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerculean'
+games{'gta5'}
+
+author 'Tazio'
+description 'staffduty script to display who goes onduty as staff'
+version 'v1.0.0'
+
+server_scripts {
+  'server.lua'
+ }
